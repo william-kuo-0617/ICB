7ae3e29c86c0f1090919074eb8d09ccda1b6b4e1
nturivengod <b02902046@ntu.edu.tw>
2017-05-05 17:59:48 +0800

    buy ~ action

--- episoda.py
+++ episoda.py
@@ -8,16 +8,16 @@ import pandas as pd
 def initializer(episode1):
     init_temp = []
     if episode1.goal == 'Exchange':
-        init_temp = ["Please give me the exchange rate of "+episode1.inform_slots['country1']+" to "+episode1.inform_slots['country2']+"\n","I’d like to know the exchange rate of "+episode1.inform_slots['country1']+" to "+episode1.inform_slots['country2']+"\n"]
+        init_temp = ["Please give me the exchange rate of "+episode1.inform_slots['country1']+" to "+episode1.inform_slots['country2']+"\n","I'd like to know the exchange rate of "+episode1.inform_slots['country1']+" to "+episode1.inform_slots['country2']+"\n"]
         init_temp.append(str("What is the exchange rate of "+episode1.inform_slots['country2']+"?"))
-        init_temp.append(str("How’s the exchange rate of "+episode1.inform_slots['country2']+"?"))
+        init_temp.append(str("How's the exchange rate of "+episode1.inform_slots['country2']+"?"))
         init_temp.append(str("Give me the exchange rate"))
-        init_temp.append(str("I’d like to know the exchange rate"))
+        init_temp.append(str("I'd like to know the exchange rate"))
         init_temp.append(str("Exchange rate, please."))
         init_temp.append(str("The exchange rate between "+episode1.inform_slots['country1']+" and "+episode1.inform_slots['country2']+"."))
         return random.choice(init_temp)
     elif episode1.goal == 'Query':
-        init_temp = ["Please give me the share price detail of "+episode1.inform_slots['symbol']+" "+str(episode1.inform_slots['date'])+"\n", "What is the close price of "+episode1.inform_slots['symbol']+" on "+str(episode1.inform_slots['date'])+"?\n", "I’d like to know the open and low price of "+episode1.inform_slots['symbol']+" on "+str(episode1.inform_slots['date'])+"\n"]
+        init_temp = ["Please give me the share price detail of "+episode1.inform_slots['symbol']+" "+str(episode1.inform_slots['date'])+"\n", "What is the close price of "+episode1.inform_slots['symbol']+" on "+str(episode1.inform_slots['date'])+"?\n", "I'd like to know the open and low price of "+episode1.inform_slots['symbol']+" on "+str(episode1.inform_slots['date'])+"\n"]
         init_temp.append(str("Give me "+str(episode1.inform_slots['date'])+"'s stock price"))
         init_temp.append(str("the information of "+episode1.inform_slots['company_name']+", please"))
         init_temp.append(str("I would like to see some stock information"))
@@ -26,20 +26,20 @@ def initializer(episode1):
         init_temp.append(str("Please show me "+str(episode1.inform_slots['date'])+"'s "+episode1.inform_slots['company_name']+" stock price."))
         return random.choice(init_temp)
     elif episode1.goal == 'Get_exchange_rate':
-        init_temp = ["what’s the exchange rate of "+episode1.inform_slots['buy']+" "+episode1.inform_slots['Money_name']+" by TWD now in bank with "+episode1.inform_slots['type']+"?\n", "I want to "+episode1.inform_slots['buy']+" some "+episode1.inform_slots['Money_name']+" with "+episode1.inform_slots['type']+"?\n"]
-        init_temp.append(str("I would like to "+episode1.inform_slots['buy']+" some "+episode1.inform_slots['Money_name']+"."))
-        init_temp.append(str("I'd like to "+episode1.inform_slots['buy']+" some "+episode1.inform_slots['Money_name']+"."))
-        init_temp.append(str("I want to "+episode1.inform_slots['buy']+" some "+episode1.inform_slots['Money_name']+"."))
+        init_temp = ["what's the exchange rate of "+episode1.inform_slots['action']+" "+episode1.inform_slots['Money_name']+" by TWD now in bank with "+episode1.inform_slots['type']+"?\n", "I want to "+episode1.inform_slots['action']+" some "+episode1.inform_slots['Money_name']+" with "+episode1.inform_slots['type']+"?\n"]
+        init_temp.append(str("I would like to "+episode1.inform_slots['action']+" some "+episode1.inform_slots['Money_name']+"."))
+        init_temp.append(str("I'd like to "+episode1.inform_slots['action']+" some "+episode1.inform_slots['Money_name']+"."))
+        init_temp.append(str("I want to "+episode1.inform_slots['action']+" some "+episode1.inform_slots['Money_name']+"."))
         init_temp.append(str("I want to exchange my "+episode1.inform_slots['Money_name']+"."))
         init_temp.append(str(""))
         return random.choice(init_temp)
     elif episode1.goal == 'USDX':
         init_temp.append(str("Give me the USDX until "+str(episode1.inform_slots['time_end'])+"."))
-        init_temp.append(str("I’d like to know the USDX until "+str(episode1.inform_slots['time_end'])+"."))
+        init_temp.append(str("I'd like to know the USDX until "+str(episode1.inform_slots['time_end'])+"."))
         init_temp.append(str("I want to know the USDX until "+str(episode1.inform_slots['time_end'])+"."))
         init_temp.append(str("I'd like to know the USDX since "+str(episode1.inform_slots['time_start'])+"."))
         init_temp.append(str("I want to know the USDX since "+str(episode1.inform_slots['time_start'])+"."))
-        init_temp.append(str("I’d like to know the USDX until "+str(episode1.inform_slots['time_start'])+"."))
+        init_temp.append(str("I'd like to know the USDX until "+str(episode1.inform_slots['time_start'])+"."))
         init_temp.append(str("I'd like to know the USDX."))
         init_temp.append(str("Give me the USDX."))
         init_temp.append(str("I want to know the USDX"))
@@ -116,7 +116,7 @@ class episode(object):
     Exchange = {"country1":Country,"country2":Country}
     Query = {"symbol":symbol,"company_name":company_name,"date":None,"field":field}
     USDX = {"time_start":None,"time_end":None}
-    Get_exchange_rate = {"Money_name": Money_name, "type":["spot","cash"], "buy":["buy","sell"]}
+    Get_exchange_rate = {"Money_name": Money_name, "type":["spot","cash"], "action":["buy","sell"]}
     slots = {"Exchange":Exchange, "Query":Query, "Get_exchange_rate":Get_exchange_rate,"USDX":USDX}
     
     request = {"Exchange":("taiwan_rate","UNK"),"Query":("price_info","UNK"),"Get_exchange_rate":("ex_rate","UNK"),"USDX":("index","UNK")}
