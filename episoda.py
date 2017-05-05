a0e3ddbc427e2324c078c628f039032425da79ce
nturivengod <b02902046@ntu.edu.tw>
2017-05-05 17:39:40 +0800

    initializer

--- episoda.py
+++ episoda.py
@@ -5,6 +5,46 @@ import pandas as pd
 
 
 #from __future__ import print_function
+def initializer(episode1):
+    init_temp = []
+    if episode1.goal == 'Exchange':
+        init_temp = ["Please give me the exchange rate of "+episode1.inform_slots['country1']+" to "+episode1.inform_slots['country2']+"\n","I’d like to know the exchange rate of "+episode1.inform_slots['country1']+" to "+episode1.inform_slots['country2']+"\n"]
+        init_temp.append(str("What is the exchange rate of "+episode1.inform_slots['country2']+"?"))
+        init_temp.append(str("How’s the exchange rate of "+episode1.inform_slots['country2']+"?"))
+        init_temp.append(str("Give me the exchange rate"))
+        init_temp.append(str("I’d like to know the exchange rate"))
+        init_temp.append(str("Exchange rate, please."))
+        init_temp.append(str("The exchange rate between "+episode1.inform_slots['country1']+" and "+episode1.inform_slots['country2']+"."))
+        return random.choice(init_temp)
+    elif episode1.goal == 'Query':
+        init_temp = ["Please give me the share price detail of "+episode1.inform_slots['symbol']+" "+str(episode1.inform_slots['date'])+"\n", "What is the close price of "+episode1.inform_slots['symbol']+" on "+str(episode1.inform_slots['date'])+"?\n", "I’d like to know the open and low price of "+episode1.inform_slots['symbol']+" on "+str(episode1.inform_slots['date'])+"\n"]
+        init_temp.append(str("Give me "+str(episode1.inform_slots['date'])+"'s stock price"))
+        init_temp.append(str("the information of "+episode1.inform_slots['company_name']+", please"))
+        init_temp.append(str("I would like to see some stock information"))
+        init_temp.append(str("Show me the stock price."))
+        init_temp.append(str("I would like to see "+str(episode1.inform_slots['date'])+"'s "+episode1.inform_slots['company_name']+" stock price"))
+        init_temp.append(str("Please show me "+str(episode1.inform_slots['date'])+"'s "+episode1.inform_slots['company_name']+" stock price."))
+        return random.choice(init_temp)
+    elif episode1.goal == 'Get_exchange_rate':
+        init_temp = ["what’s the exchange rate of "+episode1.inform_slots['buy']+" "+episode1.inform_slots['Money_name']+" by TWD now in bank with "+episode1.inform_slots['type']+"?\n", "I want to "+episode1.inform_slots['buy']+" some "+episode1.inform_slots['Money_name']+" with "+episode1.inform_slots['type']+"?\n"]
+        init_temp.append(str("I would like to "+episode1.inform_slots['buy']+" some "+episode1.inform_slots['Money_name']+"."))
+        init_temp.append(str("I'd like to "+episode1.inform_slots['buy']+" some "+episode1.inform_slots['Money_name']+"."))
+        init_temp.append(str("I want to "+episode1.inform_slots['buy']+" some "+episode1.inform_slots['Money_name']+"."))
+        init_temp.append(str("I want to exchange my "+episode1.inform_slots['Money_name']+"."))
+        init_temp.append(str(""))
+        return random.choice(init_temp)
+    elif episode1.goal == 'USDX':
+        init_temp.append(str("Give me the USDX until "+str(episode1.inform_slots['time_end'])+"."))
+        init_temp.append(str("I’d like to know the USDX until "+str(episode1.inform_slots['time_end'])+"."))
+        init_temp.append(str("I want to know the USDX until "+str(episode1.inform_slots['time_end'])+"."))
+        init_temp.append(str("I'd like to know the USDX since "+str(episode1.inform_slots['time_start'])+"."))
+        init_temp.append(str("I want to know the USDX since "+str(episode1.inform_slots['time_start'])+"."))
+        init_temp.append(str("I’d like to know the USDX until "+str(episode1.inform_slots['time_start'])+"."))
+        init_temp.append(str("I'd like to know the USDX."))
+        init_temp.append(str("Give me the USDX."))
+        init_temp.append(str("I want to know the USDX"))
+        return random.choice(init_temp)
+
 
 class episode(object):
     r=requests.get('https://tw.rter.info/capi.php')
@@ -39,12 +79,12 @@ class episode(object):
         return tmp
 
     def dump(self):
-        print"{goal:",self.goal,"};"
+        print("{goal:",self.goal,"};")
         print("{inform_slots:")
         for key,val in self.inform_slots.items():
             print("\t{0}: {1}".format(key,val))
         print("};")
-        print "{request_slot:\n","\t{0}:{1}\n".format(self.request[0],self.request[1]),"};"
+        print("{request_slot:\n","\t{0}:{1}\n".format(self.request[0],self.request[1]),"};")
     def date_gen(self,start_date):
         start_date = self.start_date.toordinal() if self.start_date is not None else date.today().replace(day=1, month=1).toordinal()
         end_date = date.today().toordinal()
@@ -84,4 +124,5 @@ class episode(object):
     #diaact
 
 episode1 = episode(object)
+print(initializer(episode1))
 #import pdb;pdb.set_trace()
