264fcc400c4d37e448a526971a10191b4f8e6804
william-kuo-0617 <b02902080@ntu.edu.tw>
2017-05-06 20:28:38 +0800

    nl_response

--- rule_simulator.py
+++ rule_simulator.py
@@ -1,4 +1,3 @@
-import dialog_config
 from episoda import *
 import pdb
 class RuleSimulator(object):
@@ -8,78 +7,34 @@ class RuleSimulator(object):
         self.initialize_episode()
 
     def initialize_episode(self):
-        """ Initialize a new episode (dialog) 
-        state['history_slots']: keeps all the informed_slots
-        state['rest_slots']: keep all the slots (which is still in the stack yet)
-        """
-        self.state = {}
-        self.state['history_slots'] = {}
-        self.state['inform_slots'] = {}
-        self.state['request_slots'] = {}
-        self.state['rest_slots'] = []
-        self.state['turn'] = 0
         self.episode_over = False
         self.success = False
-        self.dialog_status = dialog_config.NO_OUTCOME_YET
-        
         self.goal = episode() #random generate a user goal
-
-
-        # self.goal['request_slots']['ticket'] = 'UNK'
-        # self.constraint_check = dialog_config.CONSTRAINT_CHECK_FAILURE
-        """ Debug: build a fake goal mannually """
-        #self.debug_falk_goal()
-        
-        # sample first action
-        user_action = {}
-        user_action['diaact'] = self.state['diaact']
-        user_action['inform_slots'] = self.state['inform_slots']
-        user_action['request_slots'] = self.state['request_slots']
-        user_action['turn'] = self.state['turn']
-        user_action['nl'] = initializer(self.goal)
-
         assert (self.episode_over != 1),' but we just started'
-        pdb.set_trace()
-
-        return user_action
 
     def next(self, system_action):
         """ Generate next User Action based on last System Action """
         
-        self.state['turn'] += 2
         self.episode_over = False
-        self.dialog_status = dialog_config.NO_OUTCOME_YET
         
         ##get system action
         sys_act = system_action['system_action']
 
-
-
-        self.state['history_slots'].update(self.state['inform_slots'])
-        self.state['inform_slots'].clear()
-
-
+        nl_response = None
         if sys_act == "inform":
-            self.response_inform(system_action)
-        elif sys_act == "multiple_choice":
-            self.response_multiple_choice(system_action)
+            nl_response = self.response_inform(system_action)
+        
         elif sys_act == "request":
-            self.response_request(system_action) 
-        elif sys_act == "closing":
-            self.response_thanks(system_action)
+            nl_response = self.response_request(system_action) 
+        
         elif sys_act == "confirm_answer":
-            self.response_confirm_answer(system_action)
+            nl_response = self.response_confirm_answer(system_action)
         elif sys_act == "response":
-            self.response_response(system_action)
+            nl_response = self.response_response(system_action)
         elif sys_act == "closing":
             self.episode_over = True
-            #self.state['diaact'] = "thanks"
-
-        #self.corrupt(self.state)
-        
-        # add NL to dia_act
-        self.add_nl_to_action(response_action)                       
-        return response_action, self.episode_over, self.dialog_status
+                               
+        return nl_response
 
     def response_response(self, system_action):
         response = []
